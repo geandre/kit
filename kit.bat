7c7697904e2bec6ae8acfa29bd9d24b55f6508dc
Geandre Miranda <geandremiranda.ms@gmail.com>
2019-05-09 17:27:25 -0400

    START

---
+++ kit.bat
@@ -0,0 +1,65 @@
+@echo off
+
+:: Check if git is ready to work
+WHERE git >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+    echo O GIT nao esta instalado.
+    echo Para baixar acesse: git-scm.com/download/win
+    goto enderror
+)
+
+if /i "%1"=="-h" goto printhelp
+if /i "%1"=="-v" goto printlogo
+if /i "%1"=="config" goto kitconfig
+
+:: End with success
+:endsuccess
+    exit /b 0
+
+:: End with error
+:enderror
+    echo.
+    echo Pressione qualquer tecla para sair...
+    pause >nul
+    exit /b 1
+
+:: Print help
+:printhelp
+    echo Ajuda do kit
+    echo Comandos basicos
+    echo    kit -v                 Exibe a versao do Kit
+    echo    kit -h                 Exibe a ajuda do Kit
+    echo    kit config             Configura o GIT para gerenciar o seu projeto
+    goto endsuccess
+
+:: Print logo
+:printlogo
+    echo ##  ##  ######  ######
+    echo ## ##     ##      ##  
+    echo ####      ##      ##  
+    echo ## ##     ##      ##  
+    echo ##  ##  ######    ##  
+    echo.
+    echo v0.1.0
+
+:: Comando config
+:kitconfig
+    echo Digite seu nome e seu primeiro sobrenome ex: Geandre Miranda
+    set /p name=:
+    echo.
+    echo Digite seu melhor email ex: aluno_dedicado@gmail.com
+    set /p email=:
+    echo.
+    echo Cole o link do projeto ex: https://github.com/geandre/wdsenac.git
+    set /p repo=:
+    echo.
+    echo Digite o nome da pasta ex: site-1
+    set /p dirname=:
+    echo.
+    echo Preparando o ambiente
+    rmdir c:\wd /s /q >nul
+    mkdir c:\wd >nul
+    chdir c:\wd >nul
+    git clone %repo% %dirname%
+    chdir c:\wd\%dirname% >nul
+    git branch
